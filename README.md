634eeb23c28dedbaf6bb3f97aaf7e7c75796d8cb
Chase Schwatka <cschwatka@gmail.com>
2022-05-09 08:29:14 -0600

    added to README, attempted war file config and final deployment

--- README.md
+++ README.md
@@ -17,8 +17,19 @@ Update Run by Id
 Delete Run by Id
 `http://localhost:8082/api/runs/2`
 
-## Description
+Search Run by Distance Range
+`http://localhost:8082/api/runs/search/distance/1/3
 
+Search Run by Name or Comment
+`http://localhost:8082/api/runs/search/high`
+
+
+## Description
+This app allows a user to log a run and keep track of useful data points such as name/location, date, distance ran, start time, end time, the calories they burned, their heart rate, etc.. The user can also add comments. The user is able to search by all text fields and by distance.
+
+## Technology Used
+Spring Data JPA repository, Spring Boot, REST in general by way of creating a backend API that accepts API requests, mysql, aws, apache.
 
 
 ## Lessons Learned
+Data JPA repos are very useful and make it easy to add findby queries. Learned how to test api endpoints. Learned about some of the quirks about Data JPA such as Optionals.
--- RunTrackerREST/src/main/java/com/skilldistillery/RunTrackerRestApplication.java
+++ RunTrackerREST/src/main/java/com/skilldistillery/RunTrackerRestApplication.java
@@ -2,12 +2,25 @@ package com.skilldistillery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+//@SpringBootApplication
+//public class RunTrackerRestApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(RunTrackerRestApplication.class, args);
+//	}
+//
+//}
 
 @SpringBootApplication
-public class RunTrackerRestApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(RunTrackerRestApplication.class, args);
-	}
-
+public class RunTrackerRestApplication extends SpringBootServletInitializer {
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(RunTrackerRestApplication.class);
+  }
+  public static void main(String[] args) {
+    SpringApplication.run(RunTrackerRestApplication.class, args);
+  }
 }
